f54d8ca614f1a73dd29299fdc23756ac65c4122e
TheKapi <72415359+TheKapi@users.noreply.github.com>
2022-12-07 18:10:34 +0100

    created angular app

---
+++ skoki/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import "../../src/styles.scss";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'skoki';
+}
