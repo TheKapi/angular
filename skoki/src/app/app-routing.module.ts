3c0335ad1ec1c6c0ccbc4a9c9378748798a45945
TheKapi <72415359+TheKapi@users.noreply.github.com>
2022-12-07 19:34:50 +0100

    added components

--- skoki/src/app/app-routing.module.ts
+++ skoki/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { RankComponent } from './components/rank/rank.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'rank', component: RankComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
